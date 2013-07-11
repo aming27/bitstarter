14101559ae70f8f2099517648c3d97d9f4a3d925
root <root@alfonso-VirtualBox.(none)>
2013-07-11 13:21:06 +0200

    version 5

--- web.js
+++ web.js
@@ -2,12 +2,15 @@
 //var app = express.createServer(express.logger());
 var fs = require ('fs');
 var FILE_DEFAULT = "index.html";
-file = file || FILE_DEFAULT;
-var buffer = new Buffer(fs.readFileSync(file));
-
-app.get('/', function(request, response) {
-  response.send(buffer);
-});
+var message = function (file){
+  file = file || FILE_DEFAULT;
+  var buffer = new Buffer(fs.readFileSync(file));
+  return (buffer.toString('utf-8');
+  }
+  
+  app.get('/', function(request, response) {
+  response.send(message());
+  });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
